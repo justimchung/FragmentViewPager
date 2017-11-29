42caeef948617355f0af4939f5141cb17d30c243
justim <justim@gmail.com>
2017-11-29 16:21:12 +0800

    在主畫面中，加入產生初始fragment的程式碼 當應用程式啟動時，先產生一個預設的fragment，讓它秀在畫面上

--- app/src/main/java/com/example/justim/myapplication/MainActivity.java
+++ app/src/main/java/com/example/justim/myapplication/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.justim.myapplication;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentTransaction transaction;
+    private FragmentManager fmag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fmag = getFragmentManager();
+        transaction = fmag.beginTransaction();
+        transaction.add(R.id.frame_contain, new FragmentUser());
+        transaction.commit();
     }
 }
