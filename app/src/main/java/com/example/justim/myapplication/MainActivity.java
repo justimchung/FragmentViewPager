9b53b58c69b57e6604c0492b70d9d60cfb9ee92e
justimchung <justim@gmail.com>
2017-11-30 06:11:44 +0800

    CatelogFragment 將引入的套件改為 import android.app.Fragment MainActivity 中替每個 radiobutton 指定 doClick 的事件處理函式

--- app/src/main/java/com/example/justim/myapplication/MainActivity.java
+++ app/src/main/java/com/example/justim/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,5 +31,34 @@ public class MainActivity extends AppCompatActivity {
         rbObject = findViewById(R.id.rbObject);
         rbSetting = findViewById(R.id.rbSetting);
         rbMessage = findViewById(R.id.rbMessage);
+    }
+
+    public void doClick(View view) {
+        transaction = fmag.beginTransaction();
+        resetAllButton();
+        switch (view.getId()) {
+            case R.id.rbPersonal:
+                rbUser.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_assignment_ind_white_24dp, 0, 0);
+                rbUser.setTextColor(getResources().getColor(R.color.colorTextEnable));
+                transaction.replace(R.id.frame_contain, new FragmentUser());
+                break;
+            case R.id.rbObject:
+                rbObject.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_free_breakfast_white_24dp, 0, 0);
+                rbObject.setTextColor(getResources().getColor(R.color.colorTextEnable));
+                transaction.replace(R.id.frame_contain, new CatelogFragment());
+                break;
+        }
+        transaction.commit();
+    }
+
+    private void resetAllButton() {
+        rbUser.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_assignment_ind_gray_24dp, 0, 0);
+        rbObject.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_free_breakfast_gray_24dp, 0, 0);
+        rbMessage.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_message_gray_24dp, 0, 0);
+        rbSetting.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_settings_gray_24dp, 0, 0);
+        rbUser.setTextColor(getResources().getColor(R.color.colorTextDisable));
+        rbObject.setTextColor(getResources().getColor(R.color.colorTextDisable));
+        rbMessage.setTextColor(getResources().getColor(R.color.colorTextDisable));
+        rbSetting.setTextColor(getResources().getColor(R.color.colorTextDisable));
     }
 }
